14ae1e851078bbe538c1f3db967f7f754a47079a
Cristóbal Paredes <cristobalgutierreznavarro@gmail.com>
2022-11-21 19:27:46 -0300

    2do Commit: Se realiza maquetación del html.

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+console.log("Hola mundo");
+
+const inputDinero = document.querySelector("#inputDinero");
+const selct = document.querySelector("#select");
+const resultadoConversor = document.querySelector("#resultadoConversor");
+
