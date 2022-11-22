a6d953a1138f77120c1ac31fe7e0a7e9429d7404
Cristóbal Paredes <cristobalgutierreznavarro@gmail.com>
2022-11-22 17:07:16 -0300

    4to Commit: Se agrega lógica de conversión de monedas.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,31 @@
-console.log("Hola mundo");
-const inputDinero = document.querySelector("#inputDinero");
-const select = document.querySelector("#select");
-const resultadoConversor = document.querySelector("#resultadoConversor");
-const selectEuro = document.querySelector("#selectEuro");
-const selctDolar = document.querySelector("#selctDolar");
-const grafico = document.querySelector("#grafico");
+const inputPesos = document.getElementById("inputPesos");
+const select = document.getElementById("select");
+const formulario = document.getElementById("formulario");
+const result = document.getElementById("result");
+const graficoDOM = document.getElementById("grafico")
+let grafico
+
+
+const obtenerMonedas = async () => {
+  try {
+    const res = await fetch(`https://mindicador.cl/api/${select.value}`);
+    const data = await res.json();
+    if (!res.ok) {
+      throw new Error("Error en la peticion");
+    }
+    const valorMoneda = +data.serie[0].valor;
+    result.textContent = `$`+(valorMoneda * inputPesos.value).toFixed();
+    return data;
+  } catch (error) {
+    result.textContent = error;
+  }
+}
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (inputPesos.value == 0 || select.value == "") {
+    alert("Debes completar el formulario");
+  } else {
+    obtenerMonedas();
+  }
+});
